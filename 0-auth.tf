d9fca21e46c7c65fc9acffd063f993a08f66caf0
Kevin Willocks <kevinwillocks@gmail.com>
2026-05-27 22:05:16 -0500

    Updated terraform block

--- 0-auth.tf
+++ 0-auth.tf
@@ -9,12 +9,10 @@ locals {
 
 #This is the begginging of the sprint.
 terraform {
-  required_version = ">= 1.10"
-
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 7.33"
     }
   }
 }
