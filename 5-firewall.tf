c385c875e02dc99cd5d5e7acc09ce46e5cb54e2a
WilliamHannah-Tech <williamhannah.tech@gmail.com>
2026-05-27 22:51:43 -0400

    firewall update

--- 5-firewall.tf
+++ 5-firewall.tf
@@ -1,28 +1,28 @@
 
 
-# resource "google_compute_firewall" "allow_http" {
-#   name    = "xxxxx-allow-http"
-#   network = google_compute_network.xxxxxxxx.name
+resource "google_compute_firewall" "allow_http" {
+  name    = "xxxxx-allow-http"
+  network = google_compute_network.xxxxxxxx.name
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["80"]
-#   }
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
 
-#   source_ranges = ["0.0.0.0/0"]
-#   target_tags   = ["xxxxx-web"]
-# }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["xxxxx-web"]
+}
 
 
-# resource "google_compute_firewall" "allow_ssh" {
-#   name    = "xxxxx-allow-ssh"
-#   network = google_compute_network.xxxxxxxx.name
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "xxxxx-allow-ssh"
+  network = google_compute_network.xxxxxxxx.name
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["22"]
-#   }
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
 
-#   source_ranges = ["0.0.0.0/0"]
-#   target_tags   = ["xxxxx-web"]
-# }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["xxxxx-web"]
+}
