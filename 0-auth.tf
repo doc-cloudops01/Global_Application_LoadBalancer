a6303b0aba424194b308c129e7a01656f7be4a1b
Kevin Willocks <kevinwillocks@gmail.com>
2026-05-27 21:59:20 -0500

    Added locals branch for group project

--- 0-auth.tf
+++ 0-auth.tf
@@ -1,3 +1,12 @@
+locals {
+  //Replace the empty strong with your project
+  bacchus_project = ""
+  dolla_bill_project = "whannah-gcp-75"
+  alex_project = ""
+  jj_project = ""
+  kevin_project = "theowaf-class7-5-kevinwillocks"
+}
+
 #This is the begginging of the sprint.
 terraform {
   required_version = ">= 1.10"
@@ -11,6 +20,6 @@ terraform {
 }
 
 provider "google" {
-  project = "whannah-gcp-75"
+  project = local.kevin_project
   zone    = "us-central1-b"
 }
