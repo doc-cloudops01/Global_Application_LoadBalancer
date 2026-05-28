25c42f96a87e788ecd2455e392a8385772510b8b
WilliamHannah-Tech <williamhannah.tech@gmail.com>
2026-05-27 21:38:05 -0400

    updated instance-temp

--- 3-intsnce-temp.tf
+++ 3-intsnce-temp.tf
@@ -1,29 +1,29 @@
 
-# resource "google_compute_instance_template" "xxxxxxx-template" {
-#   name         = "xxxxxxx-template"
-#   description  = "xxxxxxxxx"
-#   machine_type = "e2-medium"
+resource "google_compute_instance_template" "xxxxxxx-template" {
+  name         = "xxxxxxx-template"
+  description  = "xxxxxxxxx"
+  machine_type = "e2-medium"
 
-#   # Create a new disk from an image and set as boot disk
-#   disk {
-#     source_image = "debian-cloud/debian-12"
-#     boot         = true
-#     auto_delete  = true
-#   }
+  # Create a new disk from an image and set as boot disk
+  disk {
+    source_image = "debian-cloud/debian-12"
+    boot         = true
+    auto_delete  = true
+  }
 
-#   # Network Configurations 
-#   network_interface {
-#     subnetwork = google_compute_subnetwork.xxxxxxx.id
-#     access_config {
-#       # Include this section to give the VM an external IP address
-#       # }
-#     }
+  # Network Configurations 
+  network_interface {
+    subnetwork = google_compute_subnetwork.xxxxxxx.id
+    access_config {
+      # Include this section to give the VM an external IP address
+      # }
+    }
 
-#   }
+  }
 
-#   tags   = ["xxxxx-web"]
+  tags   = ["xxxxx-web"]
 
-#   # Install Webserver using file() function
-#   metadata_startup_script = file("./startup.sh")
+  # Install Webserver using file() function
+  metadata_startup_script = file("./startup.sh")
 
-# }
+}
